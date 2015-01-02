b7c105660e9901ac48fcea90d2dfccd333e5e59b
Iain Edminster <iainedminster@gmail.com>
2015-01-01 17:41:45 -0700

    Beginning work on the Cache module * Working tests for the registry and Memcache storage

---
+++ Cache/src/Resource/ResourceInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Cache\Resource;
+
+interface ResourceInterface extends \Bliss\Resource\ResourceInterface
+{
+	public function params(array $params = null);
+	
+	public function contents($contents = null);
+	
+	public function isValid($flag = null);
+}
