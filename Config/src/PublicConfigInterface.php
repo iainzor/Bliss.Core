5f3eaadee02fdcca55c46c0f7d857cb399752353
Iain Edminster <iainedminster@gmail.com>
2015-01-03 12:40:15 -0700

    Updated PublicConfigInterface to include Config instance to populate

--- Config/src/PublicConfigInterface.php
+++ Config/src/PublicConfigInterface.php
@@ -3,8 +3,5 @@ namespace Config;
 
 interface PublicConfigInterface
 {
-	/**
-	 * @return \Config\Config
-	 */
-	public function publicConfig();
+	public function populatePublicConfig(Config $config);
 }
